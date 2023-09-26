4447522378c8791b0cff1fd88b355fafcb6d1ab6
Wanlin <wanlin1@ualberta.ca>
2023-09-25 19:58:53 -0600

    minor changes

--- lab2 TCP Proxy/proxy_server.py
+++ lab2 TCP Proxy/proxy_server.py
@@ -70,4 +70,5 @@ def start_threaded_server():
             thread = Thread(target=handle_connection, args=(conn, addr))
             thread.run()
 
-start_server()
+#start_server()
+start_threaded_server()
